5f6c037b943f538fee17e53403d964fdc086b159
NazariiBodnarciuc <62654833+NazariiBodnarciuc@users.noreply.github.com>
2020-03-25 13:55:13 +0000

    Lesson 12 React JS
    https://www.youtube.com/watch?v=00ZNuBIE-pM&t=630s

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/63/Ingress_Logo.png"/>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bd08d236-12ee-40f0-a892-7904ffd78a0a/d3405iy-0d6e4d08-6a82-4afa-ab76-a557e02977a3.png/v1/fill/w_1131,h_707,strp/twitter_background_by_ahmed_art_d3405iy-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvYmQwOGQyMzYtMTJlZS00MGYwLWE4OTItNzkwNGZmZDc4YTBhXC9kMzQwNWl5LTBkNmU0ZDA4LTZhODItNGFmYS1hYjc2LWE1NTdlMDI5NzdhMy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.aoFneYcNYUNuYfdblJEqtcu180CYrztTAM6oaSeNGQg"/>
+            </div>
+            <div>
+                ava + descripton
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+        </div>
+    )
+};
+export default Profile;
