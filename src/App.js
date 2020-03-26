1b223bd9ea16ba3b52a294a894ef2bec7fad64e3
NazariiBodnarciuc <62654833+nazariibodnarciuc@users.noreply.github.com>
2020-03-26 20:05:36 +0000

    Lesson 29

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import './App.css';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
@@ -12,4 +13,35 @@ const App = () => {
     </div>   
   );
 }
+=======
+import {BrowserRouter ,Route} from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
+
+const App = (props) => {
+  return (
+      <BrowserRouter>
+          <div className="app-wrapper">
+              <Header />
+              <NavBar />
+              <div className="app-wrapper-content">
+                  <Route path='/profile'  render={ () => <Profile /> }/>
+                  <Route path='/dialogs'  render={ () => <Dialogs /> }/>
+                  <Route path='/news'     render={ () => <News /> }/>
+                  <Route path='/music'    render={ () => <Music /> }/>
+                  <Route path='/settings' render={ () => <Settings /> }/>
+              </div>
+
+          </div>
+      </BrowserRouter>
+  );
+};
+>>>>>>> Lesson 29
 export default App;
